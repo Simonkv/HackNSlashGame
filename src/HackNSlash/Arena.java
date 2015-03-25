ab462749730c112711e21c9c296b4e430c991f26
simonkvannli <simon_kvannli@hotmail.com>
2015-03-25 15:14:39 +0100

    Fix better wall painting

--- src/HackNSlash/Arena.java
+++ src/HackNSlash/Arena.java
@@ -2,11 +2,12 @@ package HackNSlash;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 public class Arena extends GameMap{
 	
 	private GamePanel panel;
-	private final int WALL_THICKNESS = 30;
+	private final int WALL_THICKNESS = 50;
 	
 	public Arena(GamePanel panel){
 		this.panel = panel;
@@ -16,12 +17,18 @@ public class Arena extends GameMap{
 	public void paint(Graphics2D g) {
 		// TODO Auto-generated method stub
 		g.setColor(Color.GREEN);
-		g.fillRect(0, 0, panel.SCREEN_WIDTH, panel.SCREEN_HEIGHT);
+		Rectangle2D.Double grass = new Rectangle2D.Double(0, 0, panel.SCREEN_WIDTH, panel.SCREEN_HEIGHT);
+		Rectangle2D.Double topWall = new Rectangle2D.Double(0, 0, WALL_THICKNESS, panel.SCREEN_HEIGHT);
+		Rectangle2D.Double leftWall = new Rectangle2D.Double(0, 0, panel.SCREEN_WIDTH, WALL_THICKNESS);
+		Rectangle2D.Double bottomWall = new Rectangle2D.Double(panel.SCREEN_WIDTH-WALL_THICKNESS, 0, WALL_THICKNESS, panel.SCREEN_HEIGHT);
+		Rectangle2D.Double rightWall = new Rectangle2D.Double(0, panel.SCREEN_HEIGHT-WALL_THICKNESS,panel.SCREEN_WIDTH, WALL_THICKNESS);
+
+		g.fill(grass);
 		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, WALL_THICKNESS, panel.SCREEN_HEIGHT);
-		g.fillRect(0, 0, panel.SCREEN_WIDTH, WALL_THICKNESS);
-		g.fillRect(panel.SCREEN_WIDTH-WALL_THICKNESS, 0, WALL_THICKNESS, panel.SCREEN_HEIGHT);
-		g.fillRect(0, panel.SCREEN_HEIGHT-WALL_THICKNESS,panel.SCREEN_WIDTH, WALL_THICKNESS);
+		g.fill(topWall);
+		g.fill(leftWall);
+		g.fill(bottomWall);
+		g.fill(rightWall);
 
 		
 	}
