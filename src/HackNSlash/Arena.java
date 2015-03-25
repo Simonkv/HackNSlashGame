5cb3616d3052f7aef49ca20e301ad3bdaa9cd43c
simonkvannli <simon_kvannli@hotmail.com>
2015-03-25 13:53:18 +0100

    Create map classes

---
+++ src/HackNSlash/Arena.java
@@ -0,0 +1,13 @@
+package HackNSlash;
+
+import java.awt.Graphics2D;
+
+public class Arena extends GameMap{
+
+	@Override
+	public void paint(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
